89d0ca237f592c7ae18b527e49c7b0e42ba23670
Dmytro <ytfantom1@gmail.com>
2024-10-24 20:21:08 +0300

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='text-center bg-neutral-600 bg-opacity-35 text-neutral-400 py-2'>
-      <div className='flex items-center justify-center gap-4'>
-        <Link to="/">About</Link>
-        <Link to="/">Contact</Link>
-      </div>
-      <p className='text-sm'>Created By Netflix Team</p>
-    </footer>
-  )
-}
+    <footer className='text-center py-6 relative z-10'>
+      <p className='text-neutral-400 text-xs tracking-wider'>
+        Created By <span className='text-white font-bold'>Netflix Team</span>
+      </p>
 
-export default Footer
+      <div className='absolute inset-0 bg-neutral-600 bg-opacity-25 backdrop-blur-md -z-10'></div>
+
+      <div className='absolute top-0 left-0 w-full h-full border-neutral-500 border-opacity-40 rounded-2xl'></div>
+    </footer>
+  );
+};
+
+export default Footer;
