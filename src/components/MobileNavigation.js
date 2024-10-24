ad5512c4ca82ff5b30deed46ef93688d74d42863
Dmytro <ytfantom1@gmail.com>
2024-10-24 20:32:53 +0300

    update mobile navigation

--- src/components/MobileNavigation.js
+++ src/components/MobileNavigation.js
@@ -4,24 +4,29 @@ import { NavLink } from 'react-router-dom'
 
 const MobileNavigation = () => {
   return (
-    <section className='lg:hidden h-14 bg-black bg-opacity-70 backdrop-blur-2xl fixed bottom-0 w-full z-40'>
-      <div className='flex items-center justify-between h-full text-neutral-400'>
-        {
-            mobileNavigation.map((nav,index)=>{
-                return(
-                    <NavLink 
-                    key={nav.label+"mobilenavigation"} 
-                    to={nav.href}
-                    className={({isActive})=>`px-3 flex h-full items-center flex-col justify-center ${isActive && "text-white"}`}
-                    >
-                        <div className='text-2xl'>
-                            {nav.icon}
-                        </div>
-                        <p className='text-sm'>{nav.label}</p>
-                    </NavLink>
-                )
-            })
-        }
+    <section className='lg:hidden h-16 bg-black bg-opacity-70 backdrop-blur-xl fixed bottom-0 w-full z-40'>
+      <div className='flex items-center justify-between h-full text-neutral-400 px-4'>
+        {mobileNavigation.map((nav, index) => (
+          <NavLink 
+            key={`${nav.label}mobilenavigation`} 
+            to={nav.href}
+            aria-label={nav.label} 
+            className={({ isActive }) => 
+              `flex flex-col items-center justify-center space-y-1 h-full w-full transition-all duration-300 ${
+                isActive ? 'text-white' : 'hover:text-neutral-200'
+              }`
+            }
+          >
+
+            <div className='text-2xl'>
+              {nav.icon}
+            </div>
+
+            <p className='text-xs sm:text-sm'>
+              {nav.label}
+            </p>
+          </NavLink>
+        ))}
       </div>
     </section>
   )
