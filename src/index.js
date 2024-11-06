e170c56b9349fbd3730740c4956cd7bff9a9d050
Arturianos <127219693+Arturianos@users.noreply.github.com>
2024-11-06 16:10:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 
 /**setup axios */
-axios.defaults.baseURL = "/api";
+axios.defaults.baseURL = "https://api.themoviedb.org/3"
 axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
